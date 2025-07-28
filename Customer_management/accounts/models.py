a00bb4507bd19a12fe51eb426ad4842e30f9b694
Gucha_duncan <gucha_duncan@outlook.com>
2025-07-28 16:20:08 +0300

    customer model and product model added

--- Customer_management/accounts/models.py
+++ Customer_management/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length= 150, null= True)
+    phone = models.IntegerField(max_length= 20, null= True)
+    email = models.EmailField(max_length= 200, null= True)
+    day_created = models.DateField(auto_now_add= True, null = True)
+
+class Product(models.Model):
+    
+    name = models.CharField(max_length= 100, null = True)
+    price = models.FloatField(max_length= 200, null= True)
