23952079180a75705aa1d87a4f82f698e880f167
Gucha_duncan <gucha_duncan@outlook.com>
2025-07-28 16:54:10 +0300

    Orders model implemented. also models relationships put in place

--- Customer_management/accounts/models.py
+++ Customer_management/accounts/models.py
@@ -3,11 +3,40 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     name = models.CharField(max_length= 150, null= True)
-    phone = models.IntegerField(max_length= 20, null= True)
+    phone = models.IntegerField( null= True)
     email = models.EmailField(max_length= 200, null= True)
-    day_created = models.DateField(auto_now_add= True, null = True)
+    day_created = models.DateTimeField(auto_now_add= True, null = True)
+    
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     
+    PRODUCT_CATEGORY = [
+        ('Electronics', 'Electronics'),
+        ('Clothing', 'Clothing'),
+        ('Home Appliances', 'Home Appliances'),
+        ('Books', 'Books'),
+        ('Beauty & Personal Care', 'Beauty & Personal Care')
+    ]
+    
     name = models.CharField(max_length= 100, null = True)
-    price = models.FloatField(max_length= 200, null= True)
+    price = models.FloatField(null= True)
+    category = models.CharField(max_length= 200, null = True, choices= PRODUCT_CATEGORY)
+    description = models.CharField(max_length= 500, null= True)
+    date_created = models.DateTimeField(auto_now_add= True, null= True)
+    def __str__(self):
+        return self.name
+    
+class Orders(models.Model):
+    ORDER_STATUS = [
+        ('pending', 'Pending'),
+        ('Delivered', 'Delivered'),
+        ('Out for Delivery', 'Out for Delivery'),
+    ]
+    Customer = models.ForeignKey(Customer, on_delete= models.CASCADE)
+    product = models.ManyToManyField(Product, )
+    date_created = models.DateTimeField(auto_now_add= True, null= True)
+    status = models.CharField(max_length= 500, null= True, choices= ORDER_STATUS)
+    def __str__(self):
+        return self.status
