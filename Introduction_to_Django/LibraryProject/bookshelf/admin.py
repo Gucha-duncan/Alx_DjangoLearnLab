d3c2a332eb13398fbf9d2d7bee1d6ae39276a9f1
Gucha_duncan <gucha_duncan@outlook.com>
2025-07-29 13:56:45 +0300

    Created super user

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-from .models import *
-
-
-# Register your models here.
-
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
