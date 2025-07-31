e0bce8fcbf1773a94b9f0d244ca24d15c03aab51
Gucha_duncan <gucha_duncan@outlook.com>
2025-07-31 13:24:09 +0300

    Implemented Update feature on the orders

--- Customer_management/accounts/views.py
+++ Customer_management/accounts/views.py
@@ -50,3 +50,28 @@ def createCustomer(request):
 
     context = {'form': form}
     return render(request, 'accounts/customer_form.html', context)
+
+
+def updateOrder(request,pk):
+    order = Orders.objects.get(id =pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+   
+    context ={'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request,pk):
+    order = Orders.objects.get(id =pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.delete()
+            return redirect('/')
+   
+    context ={'form':form}
+    return render(request, 'accounts/order_form.html', context)
