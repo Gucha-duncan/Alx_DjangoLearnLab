e62ece24378592a49cf517e954055f438d8306dc
Gucha_duncan <gucha_duncan@outlook.com>
2025-07-31 17:44:17 +0300

    Login page implemented

--- Customer_management/accounts/views.py
+++ Customer_management/accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from .models import *
 from .forms import *
+from django.contrib import messages
 # Create your views here.
 
 def registerPage(request):
@@ -11,11 +13,27 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid:
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account created for' + user)
+            return redirect('login')
             
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+    
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect!')
+            
     context = {}
     return render(request, 'accounts/login.html', context)
     
