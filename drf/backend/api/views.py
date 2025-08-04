1c9b24588f8629de26cac65e0e20d117aa195875
Gucha_duncan <gucha_duncan@outlook.com>
2025-08-04 15:30:52 +0300

    Intro to Django Rest Framework

---
+++ drf/backend/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from products.models import *
+from django.forms.models import model_to_dict
+
+# Create your views here.
+def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data ={}
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return JsonResponse(data)
